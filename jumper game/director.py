abb59589576454b53bf19e4a1e158f9775ff6312
ManGuevara <gue21024@byui.edu>
2022-02-03 17:53:04 -0500

    director

---
+++ jumper game/director.py
@@ -0,0 +1,30 @@
+class Director:
+    pass
+
+    def__init__(self):
+        pass
+
+    """start the game"""
+    """takes the word from a list that is return from getting word class"""
+
+    """analyse if the input letter is correct or not"""
+
+    """it maneges the game to continue if the word is correct or not"""
+
+    """if the word is completed the game is over"""
+
+    """it receives an x from the parachute class in order to finish the game """
+
+
+    def start_game(self):
+
+        """start the game"""
+    
+    def update_word(self):
+        """takes the word input from a list """
+        """analyse if the input letter is correct or not"""
+        """it maneges the game to continue if the word is correct or not"""
+    
+    def finish_game(self):
+        """"if the word is completed the game is over"""
+        """it receives an x from the parachute class in order to finish the game"""
