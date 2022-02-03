76e2244bf24396edc7231f54ec845d678eb425b5
ManGuevara <gue21024@byui.edu>
2022-02-03 17:41:18 -0500

    getting_word

---
+++ jumper game/getting_word.py
@@ -0,0 +1,19 @@
+class Get_word:
+    pass
+    def __init__(self):
+        pass
+
+    """it creates a list of words"""
+    """it takes just one random word"""
+    """it takes the letters from the random word and put them in underscores"""
+
+
+    def create_words(self):
+
+        """"method for creating a list of words"""
+
+    def random_word(self):
+        """pick a random word"""
+
+    def putting_undescores(self):
+        """shows the hiden word in underscores"""
