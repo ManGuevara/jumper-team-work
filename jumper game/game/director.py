27e8068295d84546372245a44608a7fc527799f3
lcromanini <luiscarloromanini@gmail.com>
2022-02-05 15:50:18 -0300

    Classes Director and Word (getting_word).

--- jumper game/game/director.py
+++ jumper game/game/director.py
@@ -1,30 +1,47 @@
 class Director:
-    pass
+    """A person who directs the game. 
+    
+    The responsibility of a Director is to control the sequence of play.
 
+    Attributes:
+        hider (Hider): The game's hider.
+        is_playing (boolean): Whether or not to keep playing.
+        seeker (Seeker): The game's seeker.
+        terminal_service: For getting and displaying information on the terminal.
+    """
     def __init__(self):
         pass
-
-    """start the game"""
-    """takes the word from a list that is return from getting word class"""
-
-    """analyse if the input letter is correct or not"""
-
-    """it maneges the game to continue if the word is correct or not"""
-
-    """if the word is completed the game is over"""
-
-    """it receives an x from the parachute class in order to finish the game """
+        """Constructs a new Director.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
 
 
     def start_game(self):
+       """Starts the game by running the main game loop.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+    
+    def _get_inputs(self):
+        """Gets a word from the user.
 
-        """start the game"""
+        Args:
+            self (Director): An instance of Director.
+        """
+
+    def _do_updates(self):
+        """Checks if the word has the letter entered by the user
+        Args:
+            self (Director): An instance of Director.
+        """
+        
+    def _do_outputs(self):
+        """Provides a feedback for the player to use. The entered letter is in the word or not.
+
+        Args:
+            self (Director): An instance of Director.
+        """ 
     
-    def update_word(self):
-        """takes the word input from a list """
-        """analyse if the input letter is correct or not"""
-        """it maneges the game to continue if the word is correct or not"""
-    
-    def finish_game(self):
-        """"if the word is completed the game is over"""
-        """it receives an x from the parachute class in order to finish the game"""
