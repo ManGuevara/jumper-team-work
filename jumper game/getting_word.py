9c7dfa58b738a572b86cfa835932f826f572aea8
lcromanini <43241782+lcromanini@users.noreply.github.com>
2022-02-05 21:53:20 -0300

    Uptaded class Word

--- jumper game/getting_word.py
+++ jumper game/getting_word.py
@@ -1,19 +1,35 @@
-class Get_word:
-    pass
+import random
+class Word:
+    """ Random word from a list of words
+    The responsibility of Word is to provide a random word from a list of words
+    
+   
+    Attributes:
+        value (int): The number of spots on the side facing up.
+        points (int): The number of points the die is worth.
+    """
+
     def __init__(self):
-        pass
+        """
+        Constructs a new instance of Word with a value attribute.
+        Args:
+            self (Word): An instance of Word.
+        
+        """
 
-    """it creates a list of words"""
-    """it takes just one random word"""
-    """it takes the letters from the random word and put them in underscores"""
 
+    def get_words(self):
 
-    def create_words(self):
-
-        """"method for creating a list of words"""
+        """ Method created to get the words from a file
+        
+        """
 
     def random_word(self):
-        """pick a random word"""
+        """ Pick a random word from the words list
+        
+        """
 
     def putting_undescores(self):
-        """shows the hiden word in underscores"""
+        """ shows the hiden word in underscores
+        
+        """
