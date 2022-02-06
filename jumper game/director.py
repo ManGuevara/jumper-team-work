9df8ebecee2424d217d8012a7e165f5bdfefc36b
lcromanini <43241782+lcromanini@users.noreply.github.com>
2022-02-05 21:51:28 -0300

    Update Director.py
    Add a few methods.

--- jumper game/director.py
+++ jumper game/director.py
@@ -1,34 +1,44 @@
 class Director:
-    pass
-
-    def__init__(self):
+    """A person who directs the game. 
+    
+    The responsibility of a Director is to control the sequence of play.
+    Attributes:
+        word (Word): The game's word provider.
+        is_playing (boolean): Whether or not to keep playing.
+        parachute (Parachute): Designed to print the parachute and its behavior
+        player (Player): The game's player.
+        
+    """
+    def __init__(self):
         pass
-
-        """A person who directs the game"""
-
-        """The responsibility of a Director is to control the sequence of play.
-
-        Attributes:
-        choose_word (choose_word): takes the word from a list that is return from word class.
-
-        check_word (check_word): analyse if the input letter is correct or not.
-
-        is_playing (boolean): it maneges the game to continue if the word is correct or not.
-
-        finish_game: (finish_game):if the word is completed the game is over.
-
-        parachute_gone (finish_game): it receives an x from the parachute class in order to finish the game 
+        """Constructs a new Director.
+        
+        Args:
+            self (Director): an instance of Director.
         """
 
-    def start_game(self):
 
-        """start the game"""
-    
-    def update_word(self):
-        """takes the word input from a list """
-        """analyse if the input letter is correct or not"""
-        """it maneges the game to continue if the word is correct or not"""
-    
-    def finish_game(self):
-        """"if the word is completed the game is over"""
-        """it receives an x from the parachute class in order to finish the game"""
+    def start_game(self):
+       """Starts the game by running the main game loop.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+
+    def _get_inputs(self):
+        """Gets a word from the user.
+        Args:
+            self (Director): An instance of Director.
+        """
+
+    def _do_updates(self):
+        """Checks if the word has the letter entered by the user
+        Args:
+            self (Director): An instance of Director.
+        """
+
+    def _do_outputs(self):
+        """Provides a feedback for the player to use. The entered letter is in the word or not.
+        Args:
+            self (Director): An instance of Director.
+        """ 
