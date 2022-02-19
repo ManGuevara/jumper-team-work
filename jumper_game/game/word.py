a8b2978501a4cdcf5ccf6d3a0a8dfaf0d0f1bf65
ManGuevara <96947666+ManGuevara@users.noreply.github.com>
2022-02-18 21:45:39 -0500

    Update word.py

--- jumper_game/game/word.py
+++ jumper_game/game/word.py
@@ -1,39 +1,32 @@
 import random
-class Word:
-    """ Random word from a list of words
-    The responsibility of Word is to provide a random word from a list of words
-    
 
-    Attributes:
-        value (int): The number of spots on the side facing up.
-        points (int): The number of points the die is worth.
-    """
-
-    
+class Words:
     def __init__(self):
-        """
-        Constructs a new instance of Word with a value attribute.
-        Args:
-            self (Word): An instance of Word.
-        
-        """
-        
-
-        """it creates a list of words"""
-        self._list_words = ["one", "two", "three"]
-        
-        """it takes just one random word"""
-        self._word= ""
-
-        """it takes the letters from the random word and put them in underscores"""
-
-
-    
-
+        '''attribute word this is the word selected by randon from a list'''    
+        self._word = " "
+       
     def random_word(self):
-        """pick a random word"""
-        index = random.randint(0,3)
-        self._word = self._list_words[index]
+        words_list = ["abruptly", "absurd", "abyss", "affix", "askew", "avenue", "awkward", "axiom",
+          "azure", "bagpipes", "bandwagon", "banjo", "bayou", "beekeeper", "bikini", "blitz", "boggle",
+          "bookworm", "boxcar", "boxful", "buckaroo", "buffalo", "buffoon", "buxom", "buzzard", "buzzing",
+          "buzzwords", "breath","brick", "bridge", "bright","broken","brother","brown","brush","bucket",
+          "building", "bulb","burst","business","but","butter","button","cake","camera","canvas" "card","care",
+          "carriage", "caliph", "cobweb", "cockiness", "crypt", "cycle", "daiquiri", "disavow", "dizzying",
+          "duplex", "dwarves", "embezzle", "equip", "espionage", "empathy",
+          "exodus", "faking", "fixable", "fjord", "flapjack", "gabby", "galaxy", "galvanize", "gazebo",
+          "giaour", "haiku", "haphazard", "hyphen", "icebox", "jackpot", "jaundice", "jawbreaker",
+          "jaywalk", "jazziest", "kayak", "kazoo", "keyhole", "khaki", "kilobyte", "larynx", "lengths", "lucky",
+          "luxury", "lymph", "marquis", "matrix", "megahertz", "microwave", "mnemonic", "naphtha", "nightclub",
+          "nowadays", "numbskull", "nymph", "onyx", "ovary", "oxidize", "oxygen", "pajama", "peekaboo", "phlegm",
+          "pixel", "pizazz", "quartz", "queue", "quips", "quixotic", "quiz", "razzmatazz", "rhubarb", "rhythm",
+          "rickshaw", "schnapps", "scratch", "shiv", "snazzy", "sphinx", "thriftless", "thumbscrew", "topaz",
+          "transcript", "transgress", "unknown", "unworthy", "unzip", "uptown", "vaporize", "vixen", "victory",
+          "voodoo", "vortex", "walkway", "waltz", "wave", "wavy", "wyvern", "xylophone", "yachtsman", "yippee", "yoked",
+          "youthful", "yummy", "zephyr", "zigzag", "zigzagging", "zombie"]
+       
+        randon_word = random.choice(words_list)
+        self._word = randon_word 
+        word_lenght = len(self._word)
+        self._word_lenght = word_lenght
+        return self._word
 
-    def putting_undescores(self):
-        """shows the hiden word in underscores"""
